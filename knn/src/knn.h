1dc98b4d1a0d50f1b41b6f7201b4a30a10f1e223
ciccio42 <francescorosa97@virgilio.it>
2023-04-14 18:01:16 +0200

    Update knn to novel libraries

--- knn/src/knn.h
+++ knn/src/knn.h
@@ -3,8 +3,11 @@
 
 #ifdef WITH_CUDA
 #include "cuda/vision.h"
-#include <THC/THC.h>
-extern THCState *state;
+// #include <THC/THC.h>
+// extern THCState *state;
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAEvent.h>
+cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 #endif
 
 
@@ -20,9 +23,9 @@ int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
     ref_nb = ref.size(2);
     query_nb = query.size(2);
 
-    float *ref_dev = ref.data<float>();
-    float *query_dev = query.data<float>();
-    long *idx_dev = idx.data<long>();
+    float *ref_dev = ref.data_ptr<float>();
+    float *query_dev = query.data_ptr<float>();
+    long *idx_dev = idx.data_ptr<long>();
 
 
 
@@ -30,25 +33,26 @@ int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
   if (ref.type().is_cuda()) {
 #ifdef WITH_CUDA
     // TODO raise error if not compiled with CUDA
-    float *dist_dev = (float*)THCudaMalloc(state, ref_nb * query_nb * sizeof(float));
-
+    // float *dist_dev = (float*)cudaMalloc(stream, ref_nb * query_nb * sizeof(float));
+    float *dist_dev;
+    cudaMalloc((void **)dist_dev, ref_nb * query_nb * sizeof(float));
     for (int b = 0; b < batch; b++)
     {
     // knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
     //   dist_dev, idx_dev + b * k * query_nb, THCState_getCurrentStream(state));
       knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-      dist_dev, idx_dev + b * k * query_nb, c10::cuda::getCurrentCUDAStream());
+      dist_dev, idx_dev + b * k * query_nb, at::cuda::getCurrentCUDAStream());
     }
-    THCudaFree(state, dist_dev);
+    cudaFree(dist_dev);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess)
     {
         printf("error in knn: %s\n", cudaGetErrorString(err));
-        THError("aborting");
+        // THError("aborting");
     }
     return 1;
 #else
-    AT_ERROR("Not compiled with GPU support");
+ AT_ERROR("Not compiled with GPU support");
 #endif
   }
 
